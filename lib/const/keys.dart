213ee2bcac87463df50538c55abb04ebbaa1f98d
shin <sinej1020@gmail.com>
2024-05-20 21:53:14 +0900

    rtc 엔진 추가

---
+++ lib/const/keys.dart
@@ -0,0 +1,3 @@
+const appId = 'dfdbb91266ed47b685a3c5bb638916bf';
+const channelName = 'testchannel';
+const token = '007eJxTYJCJ3PM6k7NN1qbKct90m6ub/X/sFf+ituVq0EamioSbetMUGFLSUpKSLA2NzMxSU0zMk8wsTBONk02TksyMLSwNzZLSeFi80xoCGRlW/tzPwsgAgSA+N0NJanFJckZiXl5qDgMDACshIlA=';
